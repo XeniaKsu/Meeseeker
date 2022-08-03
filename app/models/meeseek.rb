7a14e28a6e58110b40ee26472e668c81d1cd77b8
Kenichi Beveridge <kenbev84@gmail.com>
2022-08-03 21:03:32 +0100

    Add Meeseeks Controller and Views and updated routes

--- app/models/meeseek.rb
+++ app/models/meeseek.rb
@@ -1,7 +1,7 @@
 class Meeseek < ApplicationRecord
   has_many :bookings
   # has_many :reviews, through: :bookings
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   # postcode_regex = /[a-z]+\d+[a-z]?\s?\d[a-z]{2}/
   # validates_presence_of :postcode, format: { with: postcode_regex }
   validates_presence_of :postcode
