5e4e1dadf459248bff68a32ab0be474241904c0f
andrew meyer <andrew@meyerhome.org>
2022-08-04 08:04:58 +0100

    Added Booking Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_200404) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_04_065351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.text "task"
+    t.date "date_available_from"
+    t.date "data_available_to"
+    t.bigint "user_id", null: false
+    t.bigint "meeseek_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["meeseek_id"], name: "index_bookings_on_meeseek_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "meeseeks", force: :cascade do |t|
     t.string "postcode"
@@ -35,5 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_200404) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "meeseeks"
+  add_foreign_key "bookings", "users"
   add_foreign_key "meeseeks", "users"
 end
