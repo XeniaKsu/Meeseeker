b562c9b364bb1516b7f2b0e2680b1ce65f5802d4
Harrie <mayhemcreates@gmail.com>
2022-08-04 11:03:53 +0100

    data seeded - postcode issue needs looking into

--- app/models/meeseek.rb
+++ app/models/meeseek.rb
@@ -1,9 +1,9 @@
 class Meeseek < ApplicationRecord
   has_many :bookings
   # has_many :reviews, through: :bookings
-  belongs_to :user, dependent: :destroy
+   belongs_to :user, dependent: :destroy
   # postcode_regex = /[a-z]+\d+[a-z]?\s?\d[a-z]{2}/
   # validates_presence_of :postcode, format: { with: postcode_regex }
   validates_presence_of :postcode
-  validates_presence_of :user
+   validates_presence_of :user
 end
