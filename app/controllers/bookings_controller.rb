5321d16c2b1c26b0a24cc439a1cb30389f6cde10
andrew meyer <andrew@meyerhome.org>
2022-08-07 09:52:48 +0100

    Changed Nav bar and made edits to show meeseeks page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,15 +17,15 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @meeseek = Meeseek.find(params[:meeseek_id])
-    @booking = Booking.new(booking_params)
-    @booking.meeseek = @meeseek
-    @booking.user = current_user
-    if @booking.save
-      # find a path to redirect a user to
+    if current_user
+      @meeseek = Meeseek.find(params[:meeseek_id])
+      @booking = Booking.new(booking_params)
+      @booking.meeseek = @meeseek
+      @booking.user = current_user
+      @booking.save
       redirect_to my_bookings_meeseeks_path
-    # else
-    #   render :new, status: :unprocessable_entity
+    else
+      redirect_to new_user_session_path
     end
   end
 
@@ -36,9 +36,7 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-
     params.require(:booking).permit(:task, :meeseek_id, :date_available_from, :data_available_to, :user_id)
-
   end
 
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,11 @@ class Booking < ApplicationRecord
   validates_presence_of :meeseek, :task
   # Need to add validation around if Meeseeks already booked during that time then not possible to book again.
   #add user to validation
+
+  # Bookings cannot be in the past
+  # def no_past_booking
+  #   if Date.today >
+  # end
+
+
 end
