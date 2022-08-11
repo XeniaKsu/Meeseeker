b64f53ac869252ec4b881cb6a981adf5f9370014
Ksenia Koneva <hello@xeniakoneva.co.uk>
2022-08-11 20:56:45 +0100

    bug fixed

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10",
-      // center: [-0, 51.5],
+      center: [-0, 51.5],
       zoom: 9
     })
 
--- app/models/meeseek.rb
+++ app/models/meeseek.rb
@@ -2,7 +2,7 @@ class Meeseek < ApplicationRecord
   has_many :bookings
   # has_many :reviews, through: :bookings
    belongs_to :user, dependent: :destroy
-  #  geocoded_by :postcode
+   geocoded_by :postcode
   #  after_validation :geocode, if: :will_save_change_to_postcode?
 
   # postcode_regex = /[a-z]+\d+[a-z]?\s?\d[a-z]{2}/
