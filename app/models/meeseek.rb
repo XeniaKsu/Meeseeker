bdf341f797e7ae7db3a719a4e5354ee17864df83
Ksenia Koneva <hello@xeniakoneva.co.uk>
2022-08-11 21:26:03 +0100

    adds after validation to meeseek model

--- app/models/meeseek.rb
+++ app/models/meeseek.rb
@@ -3,7 +3,7 @@ class Meeseek < ApplicationRecord
   # has_many :reviews, through: :bookings
    belongs_to :user, dependent: :destroy
    geocoded_by :postcode
-  #  after_validation :geocode, if: :will_save_change_to_postcode?
+   after_validation :geocode, if: :will_save_change_to_postcode?
 
   # postcode_regex = /[a-z]+\d+[a-z]?\s?\d[a-z]{2}/
   # validates_presence_of :postcode, format: { with: postcode_regex }
